bcc8b00a0eddf6de9b6f0ce6e4efe3ad1c63c2e8
Alan <dreamwalk@mail.ru>
2021-06-24 11:43:18 +0300

    Orders 0.1

--- src/routes/ordersRouter.js
+++ src/routes/ordersRouter.js
@@ -2,7 +2,10 @@ const { Router } = require("express");
 const router = Router();
 const bcrypt = require("bcrypt");
 const Orders = require("../models/orderModel");
+const { model } = require("mongoose");
 
 router.route("/clients/new").get(async (req, res) => {
   res.render("../views/ordersForm");
 });
+
+module.exports = router;
